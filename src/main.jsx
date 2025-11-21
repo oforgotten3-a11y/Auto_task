7c8ef4ed43814c73e612a1e89a83264b06b1c711
oforgotten3-a11y <oforgotten3@gmail.com>
2025-11-21 13:49:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+hereimport React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style/index.css'
@@ -6,5 +6,5 @@ import './style/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )

0fedf59490300e937ceb7ae1d2e6ca347a7166d0
oforgotten3-a11y <oforgotten3@gmail.com>
2025-11-24 17:20:13 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAuth } from '../../contexts/AuthContext' // Fixed import path
+import { useAuth } from '../contexts/AuthContext'
 
 const Dashboard = () => {
   const { user, currentBackend } = useAuth()
